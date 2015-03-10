da8e3261c8aaec92fe49a81d5d906b6a70383995
Your Name <rasmus.linden.dk@gmail.com>
2015-03-10 14:04:09 +1100

    second commit

--- static/javascripts/thinkster.js
+++ static/javascripts/thinkster.js
@@ -1,2 +1,30 @@
 angular
-  .module('thinkster', []);
+  .module('thinkster', [
+        'thinkster.config',
+        'thinkster.routes',
+        'thinkster.authentication',
+        'thinkster.layout',
+        'thinkster.posts',
+        'thinkster.utils',
+        'thinkster.profiles'
+        
+    ]);
+
+angular
+    .module('thinkster.config', []);
+
+angular
+    .module('thinkster.routes', ['ngRoute']);
+
+
+    
+angular
+  .module('thinkster')
+  .run(run);
+
+run.$inject = ['$http'];
+
+function run($http) {
+  $http.defaults.xsrfHeaderName = 'X-CSRFToken';
+  $http.defaults.xsrfCookieName = 'csrftoken';
+}
---
+++ static/javascripts/thinkster.routes.js
@@ -0,0 +1,28 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('thinkster.routes')
+    .config(config);
+
+  config.$inject = ['$routeProvider'];
+  function config($routeProvider) {
+    $routeProvider.when('/', {
+  controller: 'IndexController',
+  controllerAs: 'vm',
+  templateUrl: '/static/templates/layout/index.html'
+}).when('/+:username', {
+  controller: 'ProfileController',
+  controllerAs: 'vm',
+  templateUrl: '/static/templates/profiles/profile.html'
+}).when('/register', {
+      controller: 'RegisterController', 
+      controllerAs: 'vm',
+      templateUrl: '/static/templates/authentication/register.html'
+    }).when('/login',{
+      controller: 'LoginController',
+      controllerAs: 'vm',
+      templateUrl: '/static/templates/authentication/login.html'
+      }).otherwise('/');
+  }
+})();
